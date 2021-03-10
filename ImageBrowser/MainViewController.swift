200bd5f1fc19577036ddd2deca7b206ab4d16f81
dwkang <ehddn3821@naver.com>
2021-03-10 22:24:44 +0900

    #1 메인 화면 스토리보드 추가

---
+++ ImageBrowser/MainViewController.swift
@@ -0,0 +1,23 @@
+//
+//  ViewController.swift
+//  ImageBrowser
+//
+//  Created by dwKang on 2021/03/10.
+//
+
+import UIKit
+
+class MainViewController: UIViewController {
+
+    @IBOutlet var searchTextField: UITextField!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+    @IBAction func searchButtonClicked(_ sender: UIButton) {
+    }
+    
+}
+
